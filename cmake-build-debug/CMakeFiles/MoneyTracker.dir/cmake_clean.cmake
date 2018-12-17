6edad0324d3f0a05e9d218252aa66b372a4a35c7
Dillon Duguid <dillon.duguid@gmail.com>
2018-12-17 01:53:46 +0000

    Test commit

---
+++ cmake-build-debug/CMakeFiles/MoneyTracker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MoneyTracker.dir/main.cpp.obj"
+  "MoneyTracker.pdb"
+  "MoneyTracker.exe"
+  "MoneyTracker.exe.manifest"
+  "libMoneyTracker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MoneyTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
